e43ad69d639a724b019840c9ed9ee93c0c4bdda5
qanita1syed <qanita.bokhari@wizardscrew.com>
2022-08-26 19:23:16 +0500

    message

--- test/pageobjects/internet.js
+++ test/pageobjects/internet.js
@@ -1,20 +1,57 @@
+const { default: $ } = require('webdriverio/build/commands/browser/$');
 const Page = require('./page');
 
 class internet{
 
     get pageheader() {
         return $(`//h2[contains(text(),'Bookings')]`)
-        //return $('.account-header')
-        //return $('.account-nav-link')
     }
     get subheading() {
         return $('div.col.col-8') 
     }
     get search() {
-        //return $('.btn btn-link')
         return $('button.btn.btn-link')
-    
+        //return $("p[aceannounce]")
     }
+        get bookTickets()
+        {
+        return  $("a[data-elid='book-tickets']")
+        }
+        
+        get buyTicketsPageHeading()
+        {
+        return $ ("//h1[contains(text(),'Buy tickets to anywhere in Great Britain')]")
+         }
+
+        get from()
+        {
+           
+            return $("//body/ace-root[@id='ace-root']/div[1]/main[1]/div[1]/ace-homepage[1]/div[1]/div[1]/div[1]/ace-qtt-toggle[1]/div[1]/div[2]/div[1]/ace-qtt[1]/form[1]/div[1]/ace-qtt-station-pickers[1]/div[1]/div[1]/div[1]/div[1]/ace-station-picker[1]/div[1]/div[1]/div[1]")
+        }
+        get frmStation()
+        {
+            return $("//body/ace-root[@id='ace-root']/div[1]/main[1]/div[1]/ace-homepage[1]/div[1]/div[1]/div[1]/ace-qtt-toggle[1]/div[1]/div[2]/div[1]/ace-qtt[1]/form[1]/div[1]/ace-qtt-station-pickers[1]/div[1]/div[1]/div[1]/div[1]/ace-station-picker[1]/div[1]/div[2]")
+        }
+       
+        get pickfrom()
+        {
+            return $("//body/ace-root[@id='ace-root']/div[1]/main[1]/div[1]/ace-homepage[1]/div[1]/div[1]/div[1]/ace-qtt-toggle[1]/div[1]/div[2]/div[1]/ace-qtt[1]/form[1]/div[1]/ace-qtt-station-pickers[1]/div[1]/div[1]/div[1]/div[1]/ace-station-picker[1]/div[1]/div[2]")
+        }
+        get toStation()
+        {
+            return $("//body/ace-root[@id='ace-root']/div[1]/main[1]/div[1]/ace-homepage[1]/div[1]/div[1]/div[1]/ace-qtt-toggle[1]/div[1]/div[2]/div[1]/ace-qtt[1]/form[1]/div[1]/ace-qtt-station-pickers[1]/div[1]/div[1]/div[1]/div[2]/ace-station-picker[1]/div[1]/div[2]")
+        }
+        get searchandButBtn()
+        {
+            return $("//button[contains(text(),'Search and Buy')]")
+        }
+       
+      /*  async searchbutbtn (username, password) {
+            await this.from.setValue(username);
+            await this.pickfrom.setValue(password);
+            await this.searchandButBtn.click();
+        }
+*/
     open(path) {
         return super.open(path);
     }
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -16,31 +16,78 @@ describe('My Login application', () => {
 
     it('Page header?', async () => {
          //internet.open();
-       
-        await expect(internet.pageheader).toBeDisplayed();
+       // await expect(internet.pageheader).toBeDisplayed();
         console.log($("//h2[contains(text(),'Bookings')]").isDisplayed())
-
          })  
 
-      it('Does the page Sub-hearder is existing?', async () => {
-       // internet.open();
-       await expect (internet.subheading).toBeExisting();
-       })
-
-    it('get text for element', async () => {
+    it('get text for subheadig element', async () => {
        // await internet.open();
         let text = await $("div.col.col-8");
         await expect(text).toBeExisting()
        await expect(text).toHaveTextContaining('You have no active bookings.')
     })
-});
-describe('Interacting with elements', function () {
-    xit('get text for element', () => {
-        //browser.url('/')
-        //internet.open();
-        let text = $(".col col-8").gettext()
-        console.log(internet.subheading.toBeExisting())
-        console.log(text)
+    it('Book tickets onClick ', async () =>
+    {
+        let text = await $("a[data-elid='book-tickets']");
+        //await expect(internet.bookTickets).toBeDisplayed();
+        //await expect(internet.bookTickets).toBeExisting().click();
+        await expect(text).toBeExisting()
+        await expect(text).toHaveTextContaining('Book tickets')
+        await $("a[data-elid='book-tickets']").click();
     })
-  
-}); 
+    it('Buy Tickets Page Heding', async () =>
+    {
+        let text = await $ ("//h1[contains(text(),'Buy tickets to anywhere in Great Britain')]");
+        await expect(text).toBeExisting()
+        //await expect(text).toHaveTextContaining('Buy tickets to anywhere in Great Britain')
+       // await expect(internet.buyTicketsPageHeading).toBeExisting();
+    })
+
+    it('SELECT DESTINATION FROM', async () =>
+    {
+    let text = await $("//body/ace-root[@id='ace-root']/div[1]/main[1]/div[1]/ace-homepage[1]/div[1]/div[1]/div[1]/ace-qtt-toggle[1]/div[1]/div[2]/div[1]/ace-qtt[1]/form[1]/div[1]/ace-qtt-station-pickers[1]/div[1]/div[1]/div[1]/div[1]/ace-station-picker[1]/div[1]/div[1]/div[1]")
+    await browser.pause(3000)
+    await $("//body/ace-root[@id='ace-root']/div[1]/main[1]/div[1]/ace-homepage[1]/div[1]/div[1]/div[1]/ace-qtt-toggle[1]/div[1]/div[2]/div[1]/ace-qtt[1]/form[1]/div[1]/ace-qtt-station-pickers[1]/div[1]/div[1]/div[1]/div[1]/ace-station-picker[1]/div[1]/div[1]/div[1]").waitForExist({ timeout: 5000 })
+    await expect(text).toBeExisting()
+    await $("//body/ace-root[@id='ace-root']/div[1]/main[1]/div[1]/ace-homepage[1]/div[1]/div[1]/div[1]/ace-qtt-toggle[1]/div[1]/div[2]/div[1]/ace-qtt[1]/form[1]/div[1]/ace-qtt-station-pickers[1]/div[1]/div[1]/div[1]/div[1]/ace-station-picker[1]/div[1]/div[1]/div[1]").click();
+    await browser.pause(3000)
+
+    await browser.actions()
+    .keyDown(Key.SHIFT)
+    .sendKeys('a')
+    await browser.pause(3000)
+    })
+
+    /*it('SELECT pickFrom', async () =>
+    {
+    let text = await $("//body/ace-root[@id='ace-root']/div[1]/main[1]/div[1]/ace-homepage[1]/div[1]/div[1]/div[1]/ace-qtt-toggle[1]/div[1]/div[2]/div[1]/ace-qtt[1]/form[1]/div[1]/ace-qtt-station-pickers[1]/div[1]/div[1]/div[1]/div[1]/ace-station-picker[1]/div[1]/div[2]")
+    await expect(text).toBeExisting()
+    $("//body/ace-root[@id='ace-root']/div[1]/main[1]/div[1]/ace-homepage[1]/div[1]/div[1]/div[1]/ace-qtt-toggle[1]/div[1]/div[2]/div[1]/ace-qtt[1]/form[1]/div[1]/ace-qtt-station-pickers[1]/div[1]/div[1]/div[1]/div[1]/ace-station-picker[1]/div[1]/div[2]").selectByVisibleText("Banbury (BAN)");  
+    // $("//body/ace-root[@id='ace-root']/div[1]/main[1]/div[1]/ace-homepage[1]/div[1]/div[1]/div[1]/ace-qtt-toggle[1]/div[1]/div[2]/div[1]/ace-qtt[1]/form[1]/div[1]/ace-qtt-station-pickers[1]/div[1]/div[1]/div[1]/div[1]/ace-station-picker[1]/div[1]/div[2]").selectByAttribute(attribute, "Banbury (BAN)")
+    })
+
+    it('SELECT station from TO FIELD', async () =>
+    {
+    let text = await $("//body/ace-root[@id='ace-root']/div[1]/main[1]/div[1]/ace-homepage[1]/div[1]/div[1]/div[1]/ace-qtt-toggle[1]/div[1]/div[2]/div[1]/ace-qtt[1]/form[1]/div[1]/ace-qtt-station-pickers[1]/div[1]/div[1]/div[1]/div[2]/ace-station-picker[1]/div[1]/div[1]/div[1]")
+    await browser.pause(3000)
+    await $("//body/ace-root[@id='ace-root']/div[1]/main[1]/div[1]/ace-homepage[1]/div[1]/div[1]/div[1]/ace-qtt-toggle[1]/div[1]/div[2]/div[1]/ace-qtt[1]/form[1]/div[1]/ace-qtt-station-pickers[1]/div[1]/div[1]/div[1]/div[2]/ace-station-picker[1]/div[1]/div[1]/div[1]").waitForExist({ timeout: 5000 })
+    await expect(text).toBeExisting()
+    await $("//body/ace-root[@id='ace-root']/div[1]/main[1]/div[1]/ace-homepage[1]/div[1]/div[1]/div[1]/ace-qtt-toggle[1]/div[1]/div[2]/div[1]/ace-qtt[1]/form[1]/div[1]/ace-qtt-station-pickers[1]/div[1]/div[1]/div[1]/div[2]/ace-station-picker[1]/div[1]/div[1]/div[1]").click();
+    })
+
+    it('SELECT statin To', async () =>
+    {
+        let text = await $("//body/ace-root[@id='ace-root']/div[1]/main[1]/div[1]/ace-homepage[1]/div[1]/div[1]/div[1]/ace-qtt-toggle[1]/div[1]/div[2]/div[1]/ace-qtt[1]/form[1]/div[1]/ace-qtt-station-pickers[1]/div[1]/div[1]/div[1]/div[2]/ace-station-picker[1]/div[1]/div[1]/div[1]")
+        await expect(text).toBeExisting()
+        $("//body/ace-root[@id='ace-root']/div[1]/main[1]/div[1]/ace-homepage[1]/div[1]/div[1]/div[1]/ace-qtt-toggle[1]/div[1]/div[2]/div[1]/ace-qtt[1]/form[1]/div[1]/ace-qtt-station-pickers[1]/div[1]/div[1]/div[1]/div[2]/ace-station-picker[1]/div[1]/div[1]/div[1]").selectByVisibleText("Bicester Village (BIT)"); 
+    })
+
+    it('click on SearchAndBuyButton', async () =>
+    {
+        let text = await $("//button[contains(text(),'Search and Buy')]")
+        await expect(text).toBeExisting()
+     //await text.click()
+    //await $("//button[contains(text(),'Search and Buy')]").click();
+ 
+    }) */
+});
